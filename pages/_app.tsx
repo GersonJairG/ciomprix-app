e72abd79b15a56c128f6fd3210f6aee8565688cd
Gerson Guerrero <jairguerrerosilvera@gmail.com>
2022-12-14 20:38:37 -0500

    fix: solve details of types in _app to guards routes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,10 +7,15 @@ import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
 import { persistStore } from 'redux-persist'
 import { ReactNode } from 'react'
+import type { NextComponentType } from 'next'
+
+type CustomAppProps = AppProps & {
+  Component: NextComponentType & { Auth?: any }
+}
 
 const Noop = ({ children }: { children: ReactNode }) => <>{children}</>
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps }: CustomAppProps) {
   const Auth = Component.Auth || Noop
   const persistor = persistStore(store)
 
